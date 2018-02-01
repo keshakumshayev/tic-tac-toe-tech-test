912edcd3bd4be017d1ad80bc4427f5dfdf96356f
kkumshayev <kumshkesh@gmail.com>
2018-02-01 14:37:20 +0000

    passed unit test for tic tac toe initializing with a board generated

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -5,7 +5,24 @@ from tictactoe import TicTacToe
 class TicTacToeTest(unittest.TestCase):
     def test_initialized_with_board(self):
         game = TicTacToe()
-        self.assertEqual([], game.board)
+        self.assertEqual(1, game.board[0].row)
+        self.assertEqual(1, game.board[0].column)
+        self.assertEqual(1, game.board[1].row)
+        self.assertEqual(2, game.board[1].column)
+        self.assertEqual(1, game.board[2].row)
+        self.assertEqual(3, game.board[2].column)
+        self.assertEqual(2, game.board[3].row)
+        self.assertEqual(1, game.board[3].column)
+        self.assertEqual(2, game.board[4].row)
+        self.assertEqual(2, game.board[4].column)
+        self.assertEqual(2, game.board[5].row)
+        self.assertEqual(3, game.board[5].column)
+        self.assertEqual(3, game.board[6].row)
+        self.assertEqual(1, game.board[6].column)
+        self.assertEqual(3, game.board[7].row)
+        self.assertEqual(2, game.board[7].column)
+        self.assertEqual(3, game.board[8].row)
+        self.assertEqual(3, game.board[8].column)
 
 if __name__ == '__main__':
     unittest.main()
--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,15 @@
+from cell import Cell
+
 class TicTacToe:
     def __init__(self):
-        self.board = []
+        self.board = [
+            Cell(1,1),
+            Cell(1,2),
+            Cell(1,3),
+            Cell(2,1),
+            Cell(2,2),
+            Cell(2,3),
+            Cell(3,1),
+            Cell(3,2),
+            Cell(3,3)
+        ]
