346aef805767e9ee8ea4c178f8210996b141b997
kkumshayev <kumshkesh@gmail.com>
2018-02-02 15:41:11 +0000

    passed test for win combo array

--- tictactoe.py
+++ tictactoe.py
@@ -13,14 +13,27 @@ class TicTacToe:
             Cell(3,2),
             Cell(3,3)
         ]
+        self.win_combinations = [
+            [[1,1],[1,2],[1,3]],
+            [[2,1],[2,2],[2,3]],
+            [[3,1],[3,2],[3,3]],
+            [[1,1],[2,1],[3,1]],
+            [[1,2],[2,2],[3,2]],
+            [[1,3],[2,3],[3,3]],
+            [[1,1],[2,2],[3,3]],
+            [[1,3],[2,2],[3,1]]
+        ]
         self.turn_player = "X"
 
     def mark(self, row, column):
         if self.cell(row, column).marked() == False:
             self.cell(row, column).symbol = self.turn_player
-            self.turn_player = "O" if self.turn_player == "X" else "X"
+            self.switch_turn()
         else:
             print("Try marking an empty square")
 
     def cell(self, row, column):
         return self.board[(row-1)*3+column]
+
+    def switch_turn(self):
+        self.turn_player = "O" if self.turn_player == "X" else "X"
